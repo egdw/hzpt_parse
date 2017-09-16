ecd2e1006733f40de20bec25bf40f238420e79c3
egdw <378759617@qq.com>
2017-09-16 21:02:43 +0800

    设置可打成war文件

---
+++ src/java/com/ApplicationStartClass.java
@@ -0,0 +1,28 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * Created by hdy on 2017/9/16.
+ */
+//public class ApplicationStartClass {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ApplicationStartClass.class, args);
+//    }
+//}
+@SpringBootApplication
+public class ApplicationStartClass extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStartClass.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApplicationStartClass.class);
+    }
+}
